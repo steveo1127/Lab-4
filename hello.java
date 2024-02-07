6eff233582718f3dee282c720600fbbce2c92255
Steven F <fredriks@umich.edu>
2024-02-07 16:13:42 -0500

    Steven Add

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++) {
             System.out.println("Hello World from the new branch"+i);
-            System.out.println("Hello World from the Steven's Branch"+i);
+            System.out.println("Hello World from the Steven's Branch1"+i);
         }
     }
 }
